2355d0b9e96bb10cde53c00e2f54ed5b39469ba3
Muhaab0 <muuhaab@gmail.com>
2022-09-30 15:20:46 +0200

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Dashboard from './Components/Dashboard'
+import Sidebar from './Components/Sidebar'
+// import styled from 'styled-components'
+
+
+export default function App() {
+  return (
+    <div>
+
+    <Sidebar />
+    <Dashboard />
+
+
+    </div>
+  )
+}
+
+
+// const Div = styled.div``;
